bebbeef40179f9b9b6ccccc32b754eae8f8ca59a
HAMZA ABUABADA <abadahhamza@gmail.com>
2025-10-20 16:06:35 +0300

    feat: update override_doctype_class site config

---
+++ round_tax_amount_row_wise/migration.py
@@ -0,0 +1,7 @@
+
+from frappe.installer import update_site_config
+
+
+def after_migrate():
+    update_site_config("original_doctype_class", None)
+    update_site_config("override_doctype_class", None)
